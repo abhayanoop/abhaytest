0a6e1209589787797071b7398eb3e086e107b011
Abhay Anoop <abhayanoop1994@gmail.com>
2023-07-29 14:40:46 +0530

    Update abhay.sh

--- abhay.sh
+++ abhay.sh
@@ -2,3 +2,8 @@
 
 wget -q -O - https://github.com/bridgecrewio/yor/releases/download/0.1.183/yor_0.1.183_linux_amd64.tar.gz | tar -xvz -C /tmp
 /tmp/yor tag -d . --tag-groups git --skip-tags git_file,git_org,git_modifiers,git_last_modified_by,git_last_modified_at --parsers Terraform --tag-prefix precize_ --tag-local-modules false
+
+git config --global user.name 'My name'
+git config --global user.email 'your-username@users.noreply.github.com'
+git commit -am "Automated"
+git push
